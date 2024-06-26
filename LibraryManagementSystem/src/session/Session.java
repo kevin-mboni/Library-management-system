e67f52892ddae272ba5c1d81fc805bd02399adac
kevin-mboni <kevin.mbonigaba@amalitechtraining.org>
2024-06-26 11:51:32 +0200

    feat: implement all library functionality

---
+++ LibraryManagementSystem/src/session/Session.java
@@ -0,0 +1,13 @@
+package session;
+
+public class Session {
+    private static int patronId;
+
+    public static void setPatronId(int id) {
+        patronId = id;
+    }
+
+    public static int getPatronId() {
+        return patronId;
+    }
+}
